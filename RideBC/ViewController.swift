e5db3704d5ad78380d2bf9bef59cfb2bf2dd5b17
Barbara Cleary <clearybc@bc.edu>
2018-04-21 12:29:31 -0400

    Initial Commit

---
+++ RideBC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RideBC
+//
+//  Created by Barbara on 4/21/18.
+//  Copyright © 2018 Barbara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
